31cf3352d0c7e1ff3cf122718150d86c54c21b3f
Prakhar Gupta <gprakhar11@gmail.com>
2022-04-14 14:44:34 +0530

    Add dispatch algorithm with losses

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, json
 import numpy as np
 
-from engine import load_dispatch
+from engine import load_dispatch, load_dispatch_loss
 
 app = Flask(__name__)
+
 
 @app.route('/api', methods=['POST'])
 def engine():
@@ -16,8 +17,8 @@ def engine():
     b = np.array(request_data['b'])
     c = np.array(request_data['c'])
     PD = request_data['PD']
-    P_min = np.array([0, 0, 0])
-    P_max = np.array([PD, PD, PD])
+    P_min = np.array(request_data['pmin'])
+    P_max = np.array(request_data['pmax'])
 
     [P, C, Total_Cost] = load_dispatch(n, a, b, c, P_min, P_max, PD)
 
@@ -30,6 +31,37 @@ def engine():
         'C': C,
         "Total_Cost": Total_Cost
     }
+
+
+@app.route('/api/loss', methods=['POST'])
+def engine_with_loss():
+    """
+        Function for running engine.py
+    """
+    request_data = json.loads(request.data)
+    n = request_data['n']
+    a = np.array(request_data['a'])
+    b = np.array(request_data['b'])
+    c = np.array(request_data['c'])
+    PD = request_data['PD']
+    P_min = np.array(request_data['pmin'])
+    P_max = np.array(request_data['pmax'])
+    B = np.array(request_data['B'])
+
+    [P, PL, C, Total_Cost] = load_dispatch_loss(n, a, b, c, P_min, P_max, B, PD)
+
+    P = json.dumps(P.tolist())
+    C = json.dumps(C.tolist())
+    PL = json.dumps(PL)
+    Total_Cost = json.dumps(Total_Cost)
+
+    return {
+        'P': P,
+        'C': C,
+        'PL': PL,
+        "Total_Cost": Total_Cost
+    }
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- engine.py
+++ engine.py
@@ -1,41 +1,87 @@
 import numpy as np
+import random as random
 
-def load_dispatch(n, a, b, c, P_min, P_max, PD) :
+# def load_dispatch(n, a, b, c, P_min, P_max, PD) :
+#     """
+#         Main function
+#     """
+#     epsilon = 0.001
+#     delP = 1000
+#     lamda = max(b)
+#     P = []
+#     while abs(delP) >= epsilon:
+#         P = np.divide((lamda - b), 2*a)
+#         P = np.minimum(P, P_max)
+#         P = np.maximum(P, P_min)
+        
+#         delP = PD - np.sum(P)
+#         lamda = lamda + ((delP)/(np.sum(np.divide(1, 2*a))))
+        
+#     C = np.multiply(a, np.multiply(P, P)) + np.multiply(b, P) + c
+#     Total_Cost = np.sum(C)
+#     return [P, C, Total_Cost]
+
+
+def load_dispatch(n, a, b, c, P_min, P_max, PD):
     """
         Main function
     """
-    epsilon = 0.001
-    delP = 1000
-    lamda = max(b)
-    P = []
-    while abs(delP) >= epsilon:
-        P = np.divide((lamda - b), 2*a)
-        P = np.minimum(P, P_max)
-        P = np.maximum(P, P_min)
-        
-        delP = PD - np.sum(P)
-        lamda = lamda + ((delP)/(np.sum(np.divide(1, 2*a))))
-        
+    vis = np.ones(n)
+    P = np.zeros(n)
+    while(True):
+        f = 0
+        x = np.sum(np.multiply(vis, np.divide(1, 2*a)))
+        y = np.sum(np.multiply(vis, np.divide(b, 2*a)))
+        lamda = (PD + y) / x
+        for i in range(n):
+            if(vis[i] == 1):
+                P[i] = (lamda - b[i])/(2*a[i])
+            if(vis[i] == 1 and P[i] < P_min[i]):
+                P[i] = P_min[i]
+                PD = PD - P_min[i]
+                vis[i] = 0
+                f = 1
+            elif (vis[i] == 1 and P[i] > P_max[i]):
+                P[i] = P_max[i]
+                PD = PD - P_max[i]
+                vis[i] = 0
+                f = 1
+        if f == 0:
+            break
     C = np.multiply(a, np.multiply(P, P)) + np.multiply(b, P) + c
     Total_Cost = np.sum(C)
     return [P, C, Total_Cost]
 
 
-# # number of generating stations
-# n = 3
+def load_dispatch_loss(n, a, b, c, P_min, P_max, B, PD):
+    epsilon = 0.001
+    delP = 10
+    lamda = max(b)
+    P = []
+    PL = 0
 
-# # Cost = a*P^2 + b*P + c
-# a = np.array([0.004, 0.006, 0.009])
-# b = np.array([5.3, 5.5, 5.8]) 
-# c = np.array([500.0, 400.0, 200.0])
+    while abs(delP) >= epsilon:
+        P = np.divide((lamda - b), 2*a)
+        diff = 10
+        while diff >= epsilon:
+            P1 = P
+            # print(P)
+            for i in range(n):
+                x = 0
+                for j in range(n):
+                    if i!=j:
+                        x = x + 2 * B[i][j] * P1[j]
+                P1[i] = (1 - b[i]/lamda - x)/(2*a[i]/lamda + 2*B[i][i])
+                P1[i] = min(P1[i], P_max[i])
+                P1[i] = max(P1[i], P_min[i])
+            diff = max(abs(P1 - P))
+            P = P1
+        PL = np.dot(np.transpose(P), np.dot(B, P))
+        delP = PD + PL - np.sum(P)
 
-# PD = 800 
+        lamda = lamda + ((delP)/(np.sum(np.divide(1, 2*a))))
 
-# P_min = np.array([0, 0, 0])
-# P_max = np.array([800, 800, 800])
+    C = np.multiply(a, np.multiply(P, P)) + np.multiply(b, P) + c
+    Total_Cost = np.sum(C)
 
-# [P, C, Total_Cost] = load_dispatch(n, a, b, c, P_min, P_max, PD)
-
-# print(P)
-# print(C)
-# print(Total_Cost)
+    return [P, PL, C, Total_Cost]
